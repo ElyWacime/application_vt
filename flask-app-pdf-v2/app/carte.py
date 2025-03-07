a6dbeb0e96f38f96506d4bc188526bbb881cc276
ElyWacime <yousfi.wacime@gmail.com>
2025-03-07 22:11:40 +0100

    update map load images

--- flask-app-pdf-v2/app/carte.py
+++ flask-app-pdf-v2/app/carte.py
@@ -15,6 +15,44 @@ KOBO_AUTH = (KOBO_USERNAME, KOBO_PASS)
 BASE_URL = "https://kc-eu.kobotoolbox.org/media/original"
 
 
+
+def draw_map(data):
+    dict_geopoint_image_url={}
+    s=0
+    for key in data: 
+        if "/g" in key:    
+            geoloc=data[key]
+            coordinates = tuple(map(float, geoloc.split()[:2]))
+            image_url=data["_attachments"][s]["download_url"]
+            dict_geopoint_image_url[coordinates]=image_url
+            s+=1
+    #print(dict_geopoint_image_url)
+    #Créer une carte Folium centrée sur le premier point
+    #print(dict_geopoint_image_url.keys())
+
+    map_center = [list(dict_geopoint_image_url.keys())[0][0], list(dict_geopoint_image_url)[0][1]]
+    m = folium.Map(location=map_center, zoom_start=15)
+    for point , url in dict_geopoint_image_url.items():
+        PROXY_IMAGE_URL=url.replace("https://kc-eu.kobotoolbox.org/media/original?media_file=","http://127.0.0.1:5000/proxy_image?image=")
+        popup_html = f"""
+    <div style="width:200px">
+        <b>Image KoboToolbox</b><br>
+        <img src="{PROXY_IMAGE_URL}" style="width:100%; max-height:150px;"><br>
+        <a href="{PROXY_IMAGE_URL}">Cliquer pour afficher l\'image</a><br>
+        Cliquez à l'extérieur pour fermer
+    </div>
+    """     
+        folium.Marker(
+            location=list(point),
+            popup=folium.Popup(popup_html, max_width=250),
+            icon=folium.Icon(icon="info-sign", color="blue")
+        ).add_to(m)
+    # Sauvegarder la carte dans un fichier HTML
+    m.save("map_with_images_"+data["projet"]+".html")
+    return "map_with_images_"+data["projet"]+".html"
+
+
+'''
 def draw_map(_data):
 
     def authenticate_and_get_image_url(image_url, point):
@@ -71,3 +109,4 @@ def draw_map(_data):
     m.save("map_with_images_"+data["projet"]+".html")
     print("pdf generated at: "+"map_with_images_"+data["projet"]+".html")
     return "map_with_images_"+data["projet"]+".html"
+'''
--- flask-app-pdf-v2/app/routes.py
+++ flask-app-pdf-v2/app/routes.py
@@ -4,7 +4,12 @@ from app.my_utils2 import create_pdf_from_data
 import os
 import requests
 from app.carte import draw_map
+from dotenv import load_dotenv
 
+load_dotenv()
+KOBO_USERNAME = os.getenv("KOBO_USERNAME")
+KOBO_PASS = os.getenv("KOBO_PASSWORD")
+BASE_URL = "https://kc-eu.kobotoolbox.org/media/original"
 
 bp = Blueprint('routes', __name__)
 
@@ -28,17 +33,17 @@ def generate_pdf():
         return jsonify({"error": str(e)}), 500
     
 
-# @bp.route("/proxy_image")
-# def proxy_image():
-#     """Récupère une image protégée depuis KoboToolbox et la renvoie sans restriction"""
-#     image_path = request.args.get("image")
-#     if not image_path:
-#         return "Image path missing", 400
-#     # Construire l'URL complète de l'image
-#     full_url = f"{BASE_URL}?media_file={image_path}"
-#     # Télécharger l'image avec authentification
-#     response = requests.get(full_url, auth=(KOBO_USERNAME, KOBO_PASS))
-#     if response.status_code == 200:
-#         return Response(response.content, content_type=response.headers['Content-Type'])
-#     else:
-#         return f"Erreur {response.status_code}", response.status_code
+@bp.route("/proxy_image")
+def proxy_image():
+    """Récupère une image protégée depuis KoboToolbox et la renvoie sans restriction"""
+    image_path = request.args.get("image")
+    if not image_path:
+        return "Image path missing", 400
+    # Construire l'URL complète de l'image
+    full_url = f"{BASE_URL}?media_file={image_path}"
+    # Télécharger l'image avec authentification
+    response = requests.get(full_url, auth=(KOBO_USERNAME, KOBO_PASS))
+    if response.status_code == 200:
+        return Response(response.content, content_type=response.headers['Content-Type'])
+    else:
+        return f"Erreur {response.status_code}", response.status_code
