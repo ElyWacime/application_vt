f41f26ce90e244aecf8ca6da7bd81d28635baca8
ElyWacime <yousfi.wacime@gmail.com>
2025-03-07 21:33:49 +0100

    update

---
+++ flask-app-pdf-v2/app/carte.py
@@ -0,0 +1,73 @@
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+import folium
+import base64
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KOBO_USERNAME = os.getenv("KOBO_USERNAME")
+KOBO_PASS = os.getenv("KOBO_PASSWORD")
+KOBO_AUTH = (KOBO_USERNAME, KOBO_PASS)
+
+BASE_URL = "https://kc-eu.kobotoolbox.org/media/original"
+
+
+def draw_map(_data):
+
+    def authenticate_and_get_image_url(image_url, point):
+        # Authenticate and fetch the image URL
+        try:
+            response = requests.get(image_url, auth=KOBO_AUTH)
+            if response.status_code == 200:
+                with open(f"app/images/__{point}__.png", 'wb') as f:
+                    f.write(response.content)
+        except Exception as e:
+            print(f"Error downloading the images in the draw_image Error: {e}")
+
+    data = _data
+    dict_geopoint_image_url={}
+    s=0
+    for key in data: 
+        if "/g" in key:    
+            geoloc=data[key]
+            coordinates = tuple(map(float, geoloc.split()[:2]))
+            image_url=data["_attachments"][s]["download_url"]
+            dict_geopoint_image_url[coordinates]=image_url
+            s+=1
+    #print(dict_geopoint_image_url)
+    #Créer une carte Folium centrée sur le premier point
+    #print(dict_geopoint_image_url.keys())
+
+    map_center = [list(dict_geopoint_image_url.keys())[0][0], list(dict_geopoint_image_url)[0][1]]
+    m = folium.Map(location=map_center, zoom_start=15)
+
+    for point , url in dict_geopoint_image_url.items():
+        authenticate_and_get_image_url(url, f"{point}")
+        popup_html = f"""
+    <div style="width:200px">
+        <b>Image KoboToolbox</b><br>
+        <img src="file:///{point}" style="width:100%; max-height:150px;"><br>
+        Cliquez à l'extérieur pour fermer
+    </div>
+    """     
+    # for point , url in dict_geopoint_image_url.items():
+    #     PROXY_IMAGE_URL=url.replace("https://kc-eu.kobotoolbox.org/media/original?media_file=","http://127.0.0.1:5000/proxy_image?image=")
+    #     popup_html = f"""
+    # <div style="width:200px">
+    #     <b>Image KoboToolbox</b><br>
+    #     <img src="{PROXY_IMAGE_URL}" style="width:100%; max-height:150px;"><br>
+    #     Cliquez à l'extérieur pour fermer
+    # </div>
+    # """
+        folium.Marker(
+            location=list(point),
+            popup=folium.Popup(popup_html, max_width=250),
+            icon=folium.Icon(icon="info-sign", color="blue")
+        ).add_to(m)
+    # Sauvegarder la carte dans un fichier HTML
+    m.save("map_with_images_"+data["projet"]+".html")
+    print("pdf generated at: "+"map_with_images_"+data["projet"]+".html")
+    return "map_with_images_"+data["projet"]+".html"
--- flask-app-pdf-v2/app/routes.py
+++ flask-app-pdf-v2/app/routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, request, send_file, jsonify
+from flask import Blueprint, request, send_file, jsonify, Response
 from io import BytesIO
 from app.my_utils2 import create_pdf_from_data
 import os
+import requests
+from app.carte import draw_map
+
 
 bp = Blueprint('routes', __name__)
 
@@ -14,10 +17,28 @@ def generate_pdf():
 
     try:
         pdf_file_path = create_pdf_from_data(data)
+        map_path = draw_map(data)
         if not os.path.exists(pdf_file_path):
             return jsonify({"error": f"File not found: {pdf_file_path}"}), 500
         print(f"PDF generated at: {pdf_file_path}")
+        print(f"\n\n\n\n\n{map_path}\n\n\n\n\n")
         return send_file(pdf_file_path, download_name='output.pdf', as_attachment=True, mimetype='application/pdf')
     except Exception as e:
         print("\n\n\n\nENTER\n\n\n\n\n")
         return jsonify({"error": str(e)}), 500
+    
+
+# @bp.route("/proxy_image")
+# def proxy_image():
+#     """Récupère une image protégée depuis KoboToolbox et la renvoie sans restriction"""
+#     image_path = request.args.get("image")
+#     if not image_path:
+#         return "Image path missing", 400
+#     # Construire l'URL complète de l'image
+#     full_url = f"{BASE_URL}?media_file={image_path}"
+#     # Télécharger l'image avec authentification
+#     response = requests.get(full_url, auth=(KOBO_USERNAME, KOBO_PASS))
+#     if response.status_code == 200:
+#         return Response(response.content, content_type=response.headers['Content-Type'])
+#     else:
+#         return f"Erreur {response.status_code}", response.status_code
